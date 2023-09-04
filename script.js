9818440a029230688a89bf960568607a42ae7956
Harshitn08 <121488937+Harshitn08@users.noreply.github.com>
2023-09-05 01:24:00 +0530

    Fixed indentation in script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function myFunction() {
     document.querySelector("h1").classList.toggle("p2");
-var randomVar1 = Math.floor(Math.random()*6) + 1;
-var randomVar2 = Math.floor(Math.random()*6) + 1;
+    var randomVar1 = Math.floor(Math.random()*6) + 1;
+    var randomVar2 = Math.floor(Math.random()*6) + 1;
 
 
-var img1 = document.getElementsByClassName("img1")[0];
-img1.setAttribute("src","./dice" + randomVar1 + ".png");
+    var img1 = document.getElementsByClassName("img1")[0];
+    img1.setAttribute("src","./dice" + randomVar1 + ".png");
 
-var img2 = document.getElementsByClassName("img2")[0];
-img2.setAttribute("src","./dice" + randomVar2 + ".png");
+    var img2 = document.getElementsByClassName("img2")[0];
+    img2.setAttribute("src","./dice" + randomVar2 + ".png");
 
-var heading = document.getElementById("heading");
+    var heading = document.getElementById("heading");
 
 
-if(randomVar1>randomVar2)
-{
-    heading.textContent = "Player 1 Wins";
-    heading.style.color = "red";
+    if(randomVar1 > randomVar2) {
+        heading.textContent = "Player 1 Wins";
+        heading.style.color = "red";
     }
 
-else if (randomVar1<randomVar2){
-    heading.textContent = "Player 2 Wins";
-    heading.style.color = "blue";
-}
+    else if (randomVar1 < randomVar2) {
+        heading.textContent = "Player 2 Wins";
+        heading.style.color = "blue";
+    }
 
-else {
-    heading.textContent = "It's A Draw";
-    heading.style.color = "white";
-}}
+    else {
+        heading.textContent = "It's A Draw";
+        heading.style.color = "white";
+    }
+}
